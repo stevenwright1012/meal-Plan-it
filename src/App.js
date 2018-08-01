fb6f9353910e38fb5eaee7924cd7ec320cca405f
Steven <stevenwright1012@gmail.com>
2018-08-01 10:48:54 -0600

    Sass up and running

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import router from './router';
-import './styles/reset.css';
 import './styles/App.css'
 
 class App extends Component {
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,15 +4,15 @@ class Login extends Component {
   render() {
     const {REACT_APP_LOGIN} = process.env
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <div className="Login_Container">
+        <p className="login_button">
             <a href={REACT_APP_LOGIN}>
                 LOGIN
             </a>
         </p>
+        <div className="welcome" >
+          <h1>Welcome To Meal Plan it</h1>
+        </div>
       </div>
     );
   }
